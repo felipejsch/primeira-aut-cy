f9b5ecb0496b58db1f1d5d16d12fd1235c59de3a
FELIPE SCHWEITZER <felipejsch@gmail.com>
2025-02-26 09:51:54 -0300

    Segundo Commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,18 +1,27 @@
 describe('template', () => {
-  it.skip('passes', () => {
+
+  const selectorsList = { 
+
+    usernameField: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input",
+    passwordField: ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input",
+    loginButton: ".oxd-button",
+
+  }
+
+  it('passes', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-    cy.get('.oxd-button').click()  
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()  
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index')  
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text', 'Dashboard')
    
   })
   it('fails', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Adminoooooo')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-    cy.get('.oxd-button').click()  
+    cy.get(selectorsList.usernameField).type('Adminoooooo')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()  
     cy.get('.oxd-alert') 
    
   })
